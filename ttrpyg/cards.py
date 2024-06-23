11e93bc2dde3659d7dcc05eb551569ed618d787d
ibex <thesloth00@gmail.com>
2024-06-23 00:11:05 -0700

    double_notecard

--- ttrpyg/cards.py
+++ ttrpyg/cards.py
@@ -8,7 +8,7 @@ from pylatex.package import Package
 from pylatex.utils import NoEscape
 
 import ttrpyg.my_types as ty
-import ttrpyg.text as ge # test this import
+import ttrpyg.text as ge  # test this import
 import ttrpyg.database as dt
 
 
@@ -17,40 +17,69 @@ def generate_cards(
     card_type: str = "poker",
     output_filepath: str = os.path.join("output", "cards"),
 ) -> None:
-    cards_per_page = {"quarter": 4, "tarot": 6, "poker": 9, "square": 12}
+    cards_per_page = {
+        "quarter": 4,
+        "tarot": 6,
+        "poker": 9,
+        "square": 12,
+        "double_notecard": 2,
+    }
     card_height = {
         "quarter": "130mm",
         "tarot": "110mm",
         "poker": "89mm",
         "square": "64mm",
+        "double_notecard": "2.75in",
     }
     card_width = {
         "quarter": "100mm",
         "tarot": "64mm",
         "poker": "64mm",
         "square": "64mm",
+        "double_notecard": "2.3in",
     }
     cards_per_row = {
         "quarter": 2,
         "tarot": 3,
         "poker": 3,
         "square": 3,
+        "double_notecard": 2,
     }
-    geometry_options = {"margin": ".07in"}
+
     if card_type not in cards_per_page.keys():
         raise ValueError("Invalid card type.")
+
+    if card_type == "double_notecard":
+        geometry_options = {
+            "paperwidth": "5in",
+            "paperheight": "3in",
+            "top": "0.1in",
+            "left": "0.1in",
+            "bottom": "0in",
+            "right": "0in",
+        }
+    else:
+        geometry_options = {"margin": ".07in"}
+
 
     # define the LaTex command to generate a minipage of given dimensions, and populate it with content
     class CardCommand(CommandBase):
         _latex_name = "card"
 
+    if card_type == "double_notecard":
+        minipage = (
+            NoEscape(r"\begin{minipage}[t][#1][t]{#2} #3 \end{minipage}\hspace{0.1in}"),
+        )
+    else:
+        minipage = (
+            NoEscape(r"\fbox{\begin{minipage}[t][#1][t]{#2} #3 \end{minipage}}"),
+        )
+
     card_com = UnsafeCommand(
         "newcommand",
         "\card",
         options=3,
-        extra_arguments=NoEscape(
-            r"\fbox{\begin{minipage}[t][#1][t]{#2} #3 \end{minipage}}"
-        ),
+        extra_arguments=minipage,
     )
     entity_texts = []
     for entity in entities:
